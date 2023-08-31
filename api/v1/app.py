f1f0a86583ad646563da88433ecd16d7ea705e50
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-31 13:47:51 -0700

    pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,5 +13,6 @@ app.register_blueprint(app_views)
 def close(exception=None):
     storage.close()
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(debug=True, host="0.0.0.0", port=5000, threaded=True)
