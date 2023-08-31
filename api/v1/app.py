c6dc9066c493d6ba9e6690024439d8bc699330bd
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-31 14:29:39 -0700

    handler for 404 errors

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask model"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -12,6 +12,11 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def close(exception=None):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
