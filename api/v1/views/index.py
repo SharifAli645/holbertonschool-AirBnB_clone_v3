e2a29fbe535e1e70792cd1d00e8932bd408bd29e
SharifAli645 <pandemiaonirica@gmail.com>
2023-08-31 13:47:11 -0700

    correct importation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,7 @@
 from api.v1.views.__init__ import app_views
 from flask import jsonify
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
